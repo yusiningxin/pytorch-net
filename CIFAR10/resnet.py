47e5bf8f65a91b02147ce3dfe63d8e42130a8abe
yusiningxin <448425252@qq.com>
2018-05-14 17:43:35 +0800

    add resnet

--- CIFAR10/resnet.py
+++ CIFAR10/resnet.py
@@ -13,15 +13,20 @@ import torch.nn.functional as F
 
 class BasicBlock(nn.Module):
     expansion = 1
-
+    # in_planes: 输入时channel(feature map)数量
+    # planse: 卷积核个数
+    #注意padding=1不是0
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
+        #每个BasicBlock只有第一层的stride可能不为1
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
+        #当stride！=1时 输入x的size大小一定不等于out的size大小，所以需要调整
+        #同时输入和输出的channel数也可能不同，用1x1的卷积来调整，对应的stride等于传入的stride，调整size。这样对应论文中的方程2--用权重W调整
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
@@ -41,15 +46,19 @@ class Bottleneck(nn.Module):
 
     def __init__(self, in_planes, planes, stride=1):
         super(Bottleneck, self).__init__()
+        #第一层是1x1卷积调整维度
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
+        #最后一层主要是利用1x1卷积进行扩充维度
         self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
         self.bn3 = nn.BatchNorm2d(self.expansion*planes)
 
         self.shortcut = nn.Sequential()
+        #stride决定了输入输出w和h是否相同，in_planes和self.expansion*planes决定了channel是否相同
         if stride != 1 or in_planes != self.expansion*planes:
+            #利用1x1卷积进行调整，同时stride等于传入的stride
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(self.expansion*planes)
@@ -68,16 +77,20 @@ class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10):
         super(ResNet, self).__init__()
         self.in_planes = 64
-
+        #先接一个3*3的卷积结构，原论文中似乎是接的7*7卷积，注意padding=1
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
+        #参考论文中34层结构，以下为4大块的残差结构，每一块的残差结构是由若干个BasicBlock组成的。
+        #单样本大小：64x32x32
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        #单样本大小：64*32*32
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
         self.linear = nn.Linear(512*block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
+        # num_blocks表示需要有多少个BasicBlock
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
         for stride in strides:
@@ -100,9 +113,11 @@ class ResNet(nn.Module):
 def ResNet18():
     return ResNet(BasicBlock, [2,2,2,2])
 
+#BasicBlock中block.expansion为1，该实现对应论文中的34层结构，可以参考。
 def ResNet34():
     return ResNet(BasicBlock, [3,4,6,3])
 
+#Bottleneck和BasicBlock的区别参考论文中Table1
 def ResNet50():
     return ResNet(Bottleneck, [3,4,6,3])
 
@@ -114,7 +129,7 @@ def ResNet152():
 
 
 def test():
-    net = ResNet18()
+    net = ResNet50()
     y = net(torch.randn(1,3,32,32))
     print(y.size())
 
